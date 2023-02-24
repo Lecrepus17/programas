8c6d34be6f378a14cc92d281cf7b301a89a8b02b
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-02-24 09:37:47 -0300

    Initial commit

---
+++ 1702/produtos.php
@@ -0,0 +1,15 @@
+<?php
+# produtos.php
+
+    require('vendor/autoload.php');
+        
+    $loader = new \Twig\Loader\FilesystemLoader('./templates');
+
+    $twig = new \Twig\Environment($loader);
+
+    $template = $twig->load('produtos.html');
+
+    
+    echo $template->render([
+        'titulo' => 'titulo',
+   ]);
